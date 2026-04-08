e6bd079916738ea9fdc7dfb83dcf9c95853987a6
Vijay_Prajapati <vijay9112004@gmail.com>
2026-04-08 18:06:55 +0530

    Update pageRoutes.js

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router  = express.Router();
 const path    = require("path");
 
+router.get("/", (req, res) => {
+  res.send("AI Jailbreak Arena is live 🚀");
+});
+
 router.get("/leaderboard", (req, res) =>
     res.sendFile(path.join(__dirname, "../public", "leaderboard.html")));
 
